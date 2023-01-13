52b8884eeb7a8fdae5959f2e161aeb650e43716e
Mehmet Arslan <marslan.developer@gmail.com>
2023-01-13 12:05:41 +0300

    first day

--- main/settings/base.py
+++ main/settings/base.py
@@ -209,3 +209,8 @@ REST_AUTH_SERIALIZERS = {
     'TOKEN_SERIALIZER': 'users.serializers.CustomTokenSerializer',
     
 }
+
+# AUTH_USER_MODEL = 'users.Myuser'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "pictures"
